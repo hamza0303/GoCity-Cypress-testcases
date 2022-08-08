17149874e87325329daa66bd825b5486ce63e4d5
hshafiq <hshafiq@NIS-PK640-MacBook-Pro.local>
2022-08-08 21:04:07 +0500

    Choose one person and click on checkout button testcase

--- test_assignment/cypress/integration/code_specs/assignment.spec.js
+++ test_assignment/cypress/integration/code_specs/assignment.spec.js
@@ -40,5 +40,15 @@ describe("QA Engineer Code Challenge Assignment..", () => {
         expect(selected_pass_text).to.equal("1 day pass"); //expected 1 day pass to equal 1 day pass on page
       });
     });
-   
+    it("Choose for one person and click on “Checkout” button", () => {
+      cy.get(".lc-cart__item-amount-value")
+        .first()
+        .then((person_amount) => {
+          const person_amount_count = person_amount.text(); //Fetch the value and convert into int
+          expect(parseInt(person_amount_count)).to.equal(1); //expected 1 to equal to 1
+        });
+      cy.wait(2000);
+      cy.get("a").contains("Checkout").click({ force: true }); //fire the event at the checkout button.
+      cy.wait(2000);
+    });
   });
